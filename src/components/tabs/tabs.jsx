4c86d062620ab3d75a3ebed9015fe7d6704e1230
DEADushka108 <cloxx108@gmail.com>
2020-10-07 14:37:37 +0500

    Add tabs

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,55 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class Tabs extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0,
+    };
+
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  handleTabClick(index) {
+    this.setState({
+      activeTab: index,
+    });
+  }
+
+  render() {
+    const {children} = this.props;
+    const {activeTab} = this.state;
+
+    return <div className="movie-card__desc">
+      <nav className="movie-nav movie-card__nav">
+        <ul className="movie-nav__list">
+          {children.map((child, index) => {
+            const {title} = child.props;
+
+            return <li key={`${title}-${index}`}
+              className={`movie-nav__item ${(activeTab === index) ? `movie-nav__item--active` : ``}`}
+              onClick={() => {
+                this.handleTabClick(index);
+              }}>
+              <a className="movie-nav__link">{title}</a>
+            </li>;
+          })}
+        </ul>
+      </nav>
+      {children.map((child, index) => {
+        const {children: content} = child.props;
+
+        return index === activeTab ? content : null;
+      })}
+    </div>;
+  }
+}
+
+Tabs.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
