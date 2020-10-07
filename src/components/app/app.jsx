d306f31f7ebc48bb8ef613bb0153497e15f4aa8f
DEADushka108 <cloxx108@gmail.com>
2020-10-07 11:52:47 +0500

    Make module3-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,8 @@ import UserScreen from '../user-screen/user-screen.jsx';
 import MovieScreen from '../movie-screen/movie-screen.jsx';
 import ReviewScreen from '../review-screen/review-screen.jsx';
 import PlayerScreen from '../player-screen/player-screen.jsx';
+import {findItemById} from '../../utils/utils.js';
+import {AppRoute} from '../../utils/const.js';
 
 const App = (props) => {
   const {featuredMovie, moviesList} = props;
@@ -14,27 +16,34 @@ const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
+        <Route exact path={`${AppRoute.ROOT}`} render={({history}) => (
           <Main
             featuredMovie={featuredMovie}
             moviesList={moviesList}
-          />;
-        </Route>
-        <Route exact path="/login">
+            onMovieCardClick={(movieId) => history.push(`${AppRoute.MOVIE}/${movieId}`)}
+          />
+        )}/>;
+        <Route exact path={`${AppRoute.LOGIN}`}>
           <LoginScreen />
         </Route>
-        <Route exact path="/mylist">
+        <Route exact path={`${AppRoute.FAVORITE}`}>
           <UserScreen />
         </Route>
-        <Route exact path="/review">
-          <ReviewScreen />
-        </Route>
-        <Route exact path="/films">
-          <MovieScreen movieInfo={moviesList[2]} />
-        </Route>
-        <Route exact path="/player">
-          <PlayerScreen />
-        </Route>
+        <Route exact path={`${AppRoute.MOVIE}/:id/review`} render={(routeProps) => {
+          const id = routeProps.match.params.id;
+          const movie = findItemById(id, moviesList);
+          return <ReviewScreen movieInfo={movie}/>;
+        }}/>
+        <Route exact path={`${AppRoute.MOVIE}/:id`} render={(routeProps) => {
+          const id = routeProps.match.params.id;
+          const movie = findItemById(id, moviesList);
+          return <MovieScreen movieInfo={movie}/>;
+        }}/>
+        <Route exact path={`${AppRoute.PLAYER}/:id`} render={(routeProps) => {
+          const id = routeProps.match.params.id;
+          const movie = findItemById(id, moviesList);
+          return <PlayerScreen movieInfo={movie}/>;
+        }} />
       </Switch>
     </BrowserRouter>
   );
@@ -47,11 +56,13 @@ App.propTypes = {
     releaseDate: PropTypes.number.isRequired,
   }),
   moviesList: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     genre: PropTypes.string.isRequired,
     releaseDate: PropTypes.number.isRequired,
     cover: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    previewSrc: PropTypes.string.isRequired,
     rating: PropTypes.shape({
       score: PropTypes.number.isRequired,
       level: PropTypes.string.isRequired,
--- src/components/player-screen/player-screen.jsx
+++ src/components/player-screen/player-screen.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const styles = {
   toggler: {
@@ -6,10 +7,12 @@ const styles = {
   }
 };
 
-const PlayerScreen = () => {
+const PlayerScreen = (props) => {
+  const {movieInfo} = props;
+  const {previewSrc} = movieInfo;
   return <React.Fragment>
     <div className="player">
-      <video src="#" className="player__video" poster="img/player-poster.jpg"></video>
+      <video src={previewSrc} className="player__video" poster="img/player-poster.jpg"></video>
 
       <button type="button" className="player__exit">Exit</button>
 
@@ -41,6 +44,26 @@ const PlayerScreen = () => {
       </div>
     </div>
   </React.Fragment>;
+};
+
+PlayerScreen.propTypes = {
+  movieInfo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    releaseDate: PropTypes.number.isRequired,
+    cover: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    previewSrc: PropTypes.string.isRequired,
+    rating: PropTypes.shape({
+      score: PropTypes.number.isRequired,
+      level: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+    }).isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    cast: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default PlayerScreen;
