8a82102c42d9a97a17865740303d8b45c97c6bfc
DEADushka108 <cloxx108@gmail.com>
2020-10-20 01:32:48 +0500

    Create movie-card component and connect it to redux

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,38 +1,75 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import {AppRoute} from '../../utils/const';
-import {movieDetails} from '../../types/types';
+import {connect} from 'react-redux';
+import {promoMovie} from '../../types/types';
 
 const MovieCard = (props) => {
-  const {movieInfo, children, onPlayStatusChange, onClick} = props;
-  const {id, title} = movieInfo;
-  let timeout;
+  const {featuredMovie} = props;
+  const {title, genre, releaseDate} = featuredMovie;
 
-  return <article className="small-movie-card catalog__movies-card" >
-    <div className="small-movie-card__image" onClick={() => onClick(id)}
-      onMouseEnter={() => {
-        timeout = setTimeout(onPlayStatusChange, 1000);
-      }} onMouseLeave={() => {
-        clearTimeout(timeout);
-        onPlayStatusChange();
-      }}>
-      {children}
+  return <section className="movie-card">
+    <div className="movie-card__bg">
+      <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
     </div>
-    <h3 className="small-movie-card__title">
-      <Link to={`${AppRoute.MOVIE}/${id}`} className="small-movie-card__link">{title}</Link>
-    </h3>
-  </article>;
+
+    <h1 className="visually-hidden">WTW</h1>
+
+    <header className="page-header movie-card__head">
+      <div className="logo">
+        <a className="logo__link">
+          <span className="logo__letter logo__letter--1">W</span>
+          <span className="logo__letter logo__letter--2">T</span>
+          <span className="logo__letter logo__letter--3">W</span>
+        </a>
+      </div>
+
+      <div className="user-block">
+        <div className="user-block__avatar">
+          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+        </div>
+      </div>
+    </header>
+
+    <div className="movie-card__wrap">
+      <div className="movie-card__info">
+        <div className="movie-card__poster">
+          <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218"
+            height="327"/>
+        </div>
+
+        <div className="movie-card__desc">
+          <h2 className="movie-card__title">{title}</h2>
+          <p className="movie-card__meta">
+            <span className="movie-card__genre">{genre}</span>
+            <span className="movie-card__year">{releaseDate}</span>
+          </p>
+
+          <div className="movie-card__buttons">
+            <button className="btn btn--play movie-card__button" type="button">
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                <use xlinkHref="#play-s"/>
+              </svg>
+              <span>Play</span>
+            </button>
+            <button className="btn btn--list movie-card__button" type="button">
+              <svg viewBox="0 0 19 20" width="19" height="20">
+                <use xlinkHref="#add"/>
+              </svg>
+              <span>My list</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 MovieCard.propTypes = {
-  movieInfo: movieDetails,
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]).isRequired,
-  onPlayStatusChange: PropTypes.func.isRequired,
+  featuredMovie: promoMovie,
 };
 
-export default MovieCard;
+const mapStateToProps = (state) => ({
+  featuredMovie: state.featuredMovie,
+});
+
+export {MovieCard};
+export default connect(mapStateToProps)(MovieCard);
