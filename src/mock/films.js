8866e4cbb7d1b420e328a7c911dbd0e81f07e9a3
DEADushka108 <cloxx108@gmail.com>
2020-10-20 11:59:05 +0500

    Update mock

--- src/mock/films.js
+++ src/mock/films.js
@@ -16,7 +16,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 6.6,
-      level: `Average`,
       count: 120,
     },
     description: `Macbeth, the Thane of Glamis, receives a prophecy from a trio of witches that one day he will become King of Scotland. Consumed by ambition and spurred to action by his wife, Macbeth murders his king and takes the throne for himself.`,
@@ -50,7 +49,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.5,
-      level: `Good`,
       count: 134,
     },
     description: `Kevin's mother struggles to love her strange child, despite the increasingly dangerous things
@@ -86,7 +84,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 6.6,
-      level: `Average`,
       count: 300,
     },
     description: `The second installment of the "Fantastic Beasts" series featuring the adventures of Magizoologist Newt Scamander.`,
@@ -120,7 +117,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 8.1,
-      level: `Good`,
       count: 257,
     },
     description: `In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.`,
@@ -154,7 +150,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 8.3,
-      level: `Good`,
       count: 300,
     },
     description: `Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.`,
@@ -188,7 +183,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 8.9,
-      level: `Very Good`,
       count: 578,
     },
     description: `The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.`,
@@ -222,7 +216,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 8.0,
-      level: `Good`,
       count: 1001,
     },
     description: `A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team.`,
@@ -255,8 +248,7 @@ const moviesList = [
     poster: `img/bohemian-rhapsody.jpg`,
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
-      score: 8.0,
-      level: `Good`,
+      score: 10,
       count: 240,
     },
     description: `The story of the legendary British rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Live Aid (1985).`,
@@ -290,7 +282,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.7,
-      level: `Good`,
       count: 328,
     },
     description: `A biopic depicting the early years of legendary Director and aviator Howard Hughes' career from the late 1920s to the mid 1940s.`,
@@ -324,7 +315,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.7,
-      level: `Good`,
       count: 151,
     },
     description: `Viago, Deacon and Vladislav are vampires who are finding that modern life has them struggling with the mundane - like paying rent, keeping up with the chore wheel, trying to get into nightclubs and overcoming flatmate conflicts.`,
@@ -358,7 +348,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 6.2,
-      level: `Good`,
       count: 151,
     },
     description: `After a sudden attack on MI5, Johnny English, Britain's most confident, yet unintelligent spy, becomes Britain's only spy.`,
@@ -392,7 +381,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 8.1,
-      level: `Very good`,
       count: 835,
     },
     description: `Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.`,
@@ -426,7 +414,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.8,
-      level: `Good`,
       count: 312,
     },
     description: `A pair of young lovers flee their New England town, which causes a local search party to fan out to find them.`,
@@ -460,7 +447,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.1,
-      level: `Good`,
       count: 125,
     },
     description: `True story of Heinrich Harrer, an Austrian mountain climber who became friends with the Dalai Lama at the time of China's takeover of Tibet.`,
@@ -494,7 +480,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 6.5,
-      level: `Good`,
       count: 410,
     },
     description: `As Earth is invaded by alien tripod fighting machines, one family fights for survival in this sci-fi action film.`,
@@ -528,7 +513,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 6.6,
-      level: `Good`,
       count: 73,
     },
     description: `A father and son go on the run, pursued by the government and a cult drawn to the child's special powers.`,
@@ -562,7 +546,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.2,
-      level: `Good`,
       count: 177,
     },
     description: `A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.`,
@@ -596,7 +579,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 7.2,
-      level: `Good`,
       count: 14,
     },
     description: `After Queen Elizabeth I commands him not to grow old, a young nobleman struggles with love and his place in the world.`,
@@ -630,7 +612,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 8.6,
-      level: `Very good`,
       count: 200,
     },
     description: `Set in the late 1970s, two FBI agents are tasked with interviewing serial killers to solve open cases.`,
@@ -664,7 +645,6 @@ const moviesList = [
     previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
     rating: {
       score: 6.6,
-      level: `Good`,
       count: 73,
     },
     description: `A father and son go on the run, pursued by the government and a cult drawn to the child's special powers.`,
