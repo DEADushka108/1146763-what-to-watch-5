374166aee8361573d79309b5ac961c9604e9b3bb
DEADushka108 <cloxx108@gmail.com>
2020-10-28 15:43:47 +0500

    Create user block component

---
+++ src/components/user-block/user-block.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {AppRoute} from '../../utils/const';
+import {Link} from 'react-router-dom';
+import {getAuthorization} from '../../store/user/selectors';
+import {connect} from 'react-redux';
+
+const UserBlock = (props) => {
+  const {isAuthorized} = props;
+
+  return <div className="user-block">
+    {isAuthorized ? <Link to={AppRoute.FAVORITE}><div className="user-block__avatar">
+      <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+    </div></Link> : <Link to={AppRoute.LOGIN} className="user-block__link">Sign in</Link>}
+  </div>;
+};
+
+UserBlock.propTypes = {
+  isAuthorized: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthorized: getAuthorization(state),
+});
+
+export {UserBlock};
+export default connect(mapStateToProps)(UserBlock);
