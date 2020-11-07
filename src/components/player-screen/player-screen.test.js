c4cf5e72ef0afc1511caefc183af820ae6c42767
DEADushka108 <cloxx108@gmail.com>
2020-11-07 22:36:08 +0500

    Update tests

--- src/components/player-screen/player-screen.test.js
+++ src/components/player-screen/player-screen.test.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import history from '../../routing/history';
 import {featuredMovie} from '../../__test-mock__/movies.js';
+import {MemoryRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 import PlayerScreen from './player-screen.jsx';
 
+const noop = () => {};
+const mockStore = configureStore([]);
+
 it(`Should render PlayerScreen component correctly`, () => {
+  const store = mockStore({});
   const tree = renderer
     .create(
-        <PlayerScreen
-          history={history}
-          movie={featuredMovie}
-        >
-        </PlayerScreen>
+        <Provider store={store}>
+          <MemoryRouter>
+            <PlayerScreen
+              movie={featuredMovie}
+              redirect={noop}
+            />
+          </MemoryRouter>
+        </Provider>
     )
     .toJSON();
 
