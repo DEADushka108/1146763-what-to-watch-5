68c4b5b71295b501544b8b128e08dd976608e7d8
DEADushka108 <cloxx108@gmail.com>
2020-11-09 10:51:28 +0500

    Update tests

--- src/components/player-screen/player-screen.test.js
+++ src/components/player-screen/player-screen.test.js
@@ -5,18 +5,29 @@ import {MemoryRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
 import PlayerScreen from './player-screen.jsx';
+import NameSpace from '../../store/name-space.js';
 
 const noop = () => {};
+const match = {
+  params: {
+    id: `1`,
+  }
+};
 const mockStore = configureStore([]);
 
 it(`Should render PlayerScreen component correctly`, () => {
-  const store = mockStore({});
+  const store = mockStore({
+    [NameSpace.MOVIES]: {
+      activeMovie: featuredMovie
+    }
+  });
   const tree = renderer
     .create(
         <Provider store={store}>
           <MemoryRouter>
             <PlayerScreen
-              movie={featuredMovie}
+              match={match}
+              loadMovie={noop}
               redirect={noop}
             />
           </MemoryRouter>
